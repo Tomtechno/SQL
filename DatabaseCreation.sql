716910adb0cbfd540edfdea42f98b50ec024e501
Tomtechno <69020112+Tomtechno@users.noreply.github.com>
2023-05-29 18:33:09 -0800

    Modelo

---
+++ DatabaseCreation.sql
@@ -0,0 +1,103 @@
+DROP DATABASE IF EXISTS EXCHANGE;
+CREATE DATABASE EXCHANGE;
+USE EXCHANGE;
+
+CREATE TABLE User (
+  UserId INT auto_increment PRIMARY KEY,
+  Email VARCHAR(100) UNIQUE NOT NULL,
+  Passwords VARCHAR(50) NOT NULL,
+  FirstName VARCHAR(50),
+  LastName VARCHAR(50),
+  PhoneNumber VARCHAR(50) NOT NULL,
+  Age SMALLINT NOT NULL CHECK (Age>=18),
+  CreatedTime DATETIME,
+  LastUpdatedTime DATETIME
+);
+
+CREATE TABLE Currency (
+	CurrencyId INT auto_increment PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL,
+    Symbol VARCHAR(10) NOT NULL,
+	CreatedTime DATETIME,
+	LastUpdatedTime DATETIME
+);
+
+CREATE TABLE Account (
+	AccountId INT auto_increment PRIMARY KEY,
+    UserId INT,
+    CurrencyId INT,
+    Balance DECIMAL(18, 8) NOT NULL,
+	CreatedTime DATETIME,
+	LastUpdatedTime DATETIME,
+    FOREIGN KEY (UserId) REFERENCES User (UserId),
+	FOREIGN KEY (CurrencyId) REFERENCES Currency (CurrencyId)
+);
+
+CREATE TABLE Wallet (
+	WalletId INT auto_increment  PRIMARY KEY,
+    UserId INT,
+    CurrencyId INT,
+    Balance DECIMAL(18, 8) NOT NULL,
+	CreatedTime DATETIME,
+	LastUpdatedTime DATETIME,
+    FOREIGN KEY (UserId) REFERENCES User (UserId),
+	FOREIGN KEY (CurrencyId) REFERENCES Currency (CurrencyId)
+);
+
+CREATE TABLE Transaction (
+	TxId INT auto_increment PRIMARY KEY,
+    OriginWalletId INT,
+    DestinationWalletId INT,
+    FeeCurrencyId INT,
+	Amount DECIMAL(18, 8) NOT NULL,
+	FeeAmount DECIMAL(18, 8) NOT NULL,
+	CreatedTime DATETIME,
+	LastUpdatedTime DATETIME,
+	FOREIGN KEY (OriginWalletId) REFERENCES Wallet (WalletId),
+	FOREIGN KEY (DestinationWalletId) REFERENCES Wallet (WalletId),
+	FOREIGN KEY (FeeCurrencyId) REFERENCES Currency (CurrencyId)
+);
+
+CREATE TABLE MarketPair (
+	MarketPairId INT auto_increment PRIMARY KEY,
+    BaseCurrencyId INT NOT NULL,
+    QuoteCurrencyId INT NOT NULL,
+	CreatedTime DATETIME,
+	LastUpdatedTime DATETIME,
+	FOREIGN KEY (BaseCurrencyId) REFERENCES Currency (CurrencyId),
+	FOREIGN KEY (QuoteCurrencyId) REFERENCES Currency (CurrencyId)
+);
+
+CREATE TABLE TradeOrder (
+	OrderId INT auto_increment PRIMARY KEY,
+	UserId INT,
+	MarketPairId INT,
+    BaseCurrencyId INT,
+    QuoteCurrencyId INT,
+	Side VARCHAR(10) NOT NULL,
+	OrderType VARCHAR(10) NOT NULL,
+	OrderStatus VARCHAR(20) NOT NULL,
+	CreatedTime DATETIME,
+	LastUpdatedTime DATETIME,
+	FOREIGN KEY (UserId) REFERENCES User (UserId),
+	FOREIGN KEY (MarketPairId) REFERENCES MarketPair (MarketPairId),
+	FOREIGN KEY (BaseCurrencyId) REFERENCES MarketPair (BaseCurrencyId),
+	FOREIGN KEY (QuoteCurrencyId) REFERENCES MarketPair (QuoteCurrencyId)
+);
+
+CREATE TABLE Trade (
+	TradeId INT auto_increment PRIMARY KEY,
+	OrderId INT,
+    OriginWalletId INT,
+    DestinationWalletId INT,
+    FeeCurrencyId INT,
+	Amount DECIMAL(18, 8) NOT NULL,
+    Price DECIMAL(18, 8) NOT NULL,
+	FeeAmount DECIMAL(18, 8) NOT NULL,
+	CreatedTime DATETIME,
+	LastUpdatedTime DATETIME,
+	FOREIGN KEY (OrderId) REFERENCES TradeOrder (OrderId),
+	FOREIGN KEY (OriginWalletId) REFERENCES Wallet (WalletId),
+	FOREIGN KEY (DestinationWalletId) REFERENCES Wallet (WalletId),
+	FOREIGN KEY (FeeCurrencyId) REFERENCES Currency (CurrencyId)
+);
