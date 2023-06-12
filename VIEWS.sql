ee3dac8c61ccb87a52ed4ece287f68e423fd0e65
Tomas Uzquiano <69020112+Tomtechno@users.noreply.github.com>
2023-06-12 13:51:14 -0800

    Update VIEWS.sql

--- VIEWS.sql
+++ VIEWS.sql
@@ -1,7 +1,7 @@
 USE exchange;
 
 CREATE OR REPLACE VIEW VW_Basics AS
-	(SELECT FirstName, LastName, Age, CAST(Balance AS DECIMAL(10,2)) AS Balance
+    (SELECT FirstName, LastName, Age, CAST(Balance AS DECIMAL(10,2)) AS Balance
      FROM User u JOIN Account a ON (u.userid = a.userid)
      WHERE u.Age >= 35
      ORDER BY 1,2);
@@ -9,7 +9,7 @@ CREATE OR REPLACE VIEW VW_Basics AS
 SELECT * FROM VW_Basics;
 
 CREATE OR REPLACE VIEW VW_Wallet AS
-	(SELECT WalletId, SUM(Amount*Price)
+    (SELECT WalletId, SUM(Amount*Price)
      FROM Wallet w JOIN Trade t ON (w.WalletId = t.OriginWalletId)
      GROUP BY OriginWalletId
      ORDER BY WalletId);
@@ -17,7 +17,7 @@ CREATE OR REPLACE VIEW VW_Wallet AS
 SELECT * FROM VW_Wallet;
 
 CREATE OR REPLACE VIEW VW_Currency_Search AS
-	(SELECT Name, Symbol, SUM(FeeAmount) AS FeeAmount  
+    (SELECT Name, Symbol, SUM(FeeAmount) AS FeeAmount  
      FROM Currency c JOIN Transaction t ON (c.CurrencyId = t.FeeCurrencyId)
      WHERE Name LIKE "%coin%"
      GROUP BY CurrencyId
@@ -26,7 +26,7 @@ CREATE OR REPLACE VIEW VW_Currency_Search AS
 SELECT * FROM VW_Currency_Search;
 
 CREATE OR REPLACE VIEW VW_Currency AS
-	(SELECT Symbol, Name, CAST(SUM(Amount) AS DECIMAL(10,2)) AS Amount 
+    (SELECT Symbol, Name, CAST(SUM(Amount) AS DECIMAL(10,2)) AS Amount 
      FROM Currency c JOIN Transaction t ON (c.CurrencyId = t.FeeCurrencyId)
      GROUP BY CurrencyId
      ORDER BY Symbol);
@@ -34,7 +34,7 @@ CREATE OR REPLACE VIEW VW_Currency AS
 SELECT * FROM VW_Currency;
 
 CREATE OR REPLACE VIEW VW_User_Spent AS
-	(SELECT User.FirstName, User.LastName, CAST(SUM(Trade.Amount*Trade.Price) AS DECIMAL(10,2)) AS Spent
+    (SELECT User.FirstName, User.LastName, CAST(SUM(Trade.Amount*Trade.Price) AS DECIMAL(10,2)) AS Spent
      FROM User
 	 INNER JOIN Wallet ON User.UserId = Wallet.UserId
 	 INNER JOIN Trade ON Wallet.WalletId = Trade.OriginWalletId
