3a9ba2a741c8f6b01c18706a1e2abdfd320ba8be
Tomtechno <69020112+Tomtechno@users.noreply.github.com>
2023-07-12 09:43:20 -0800

    Backup

--- Backup.sql
+++ Backup.sql
@@ -1,3 +1,3 @@
 USE exchange;
 
-mysqldump --no-create-info --skip-triggers --skip-add-drop-table --skip-lock-tables -u root -p exchange > D:\backup.sql;
+mysqldump --no-create-info --skip-triggers --skip-add-drop-table --skip-lock-tables -u root -p exchange > D:\BackupData.sql;
